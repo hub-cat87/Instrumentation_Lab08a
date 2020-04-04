36a2fe5bf29a564822d100341e2bf59536c1c031
hub-cat87 <rachel.peebles@ufl.edu>
2020-04-04 22:11:58 +0000

    something

---
+++ my-app/src/App.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ramencat from './ramencat.gif';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={ramencat} className="App-logo" alt="logo" />
+        <p>
+
+            
+        </p>
+        <p>
+          Rachel Peebles
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
